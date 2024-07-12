6fcdb1226b79de1a7768cdb152a93cccf48b8f8c
eulogioep <eulogio-ep@hotmail.com>
2024-07-12 16:08:20 +0200

    Creando el Horoscope Frangment

---
+++ app/src/main/java/com/eulogioep/horoscopoapp/ui/home/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.eulogioep.horoscopoapp.ui.home
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.eulogioep.horoscopoapp.R
+import com.eulogioep.horoscopoapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
