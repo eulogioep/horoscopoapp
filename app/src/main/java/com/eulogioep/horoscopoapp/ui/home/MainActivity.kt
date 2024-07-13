18d5124dada9bdd3a0fbbc47462435649495aac5
eulogioep <eulogio-ep@hotmail.com>
2024-07-13 06:38:37 +0200

    BottomNavigation operativo

--- app/src/main/java/com/eulogioep/horoscopoapp/ui/home/MainActivity.kt
+++ app/src/main/java/com/eulogioep/horoscopoapp/ui/home/MainActivity.kt
@@ -2,17 +2,33 @@ package com.eulogioep.horoscopoapp.ui.home
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.eulogioep.horoscopoapp.R
 import com.eulogioep.horoscopoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initUI()
 
+    }
+
+    private fun initUI() {
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navHost: NavHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHost.navController
+        binding.bottomNavView.setupWithNavController(navController)
     }
 }
